ab81fbebbe30f2491977cd3d1e1c76a18faba7c7
Jerzy Drozdz <jerzy.drozdz@gmail.com>
2018-11-02 14:49:19 +0100

    Added unit tests reproducing issue

--- tests/unit/modules/test_file.py
+++ tests/unit/modules/test_file.py
@@ -1527,6 +1527,46 @@ class FilemodLineTests(TestCase, LoaderModuleMockMixin):
             assert writelines_content[0] == expected, (writelines_content[0], expected)
 
     @with_tempfile()
+    def test_line_insert_duplicate_ensure_before(self, name):
+        '''
+        Test for file.line for insertion ensuring the line is before
+        :return:
+        '''
+        cfg_content = '/etc/init.d/someservice restart'
+        file_content = os.linesep.join([
+            '#!/bin/bash',
+            '',
+            cfg_content,
+            'exit 0'
+        ])
+        file_modified = os.linesep.join([
+            '#!/bin/bash',
+            '',
+            cfg_content,
+            'exit 0'
+        ])
+
+        isfile_mock = MagicMock(side_effect=lambda x: True if x == name else DEFAULT)
+        with patch('os.path.isfile', isfile_mock), \
+                patch('os.stat', MagicMock(return_value=DummyStat())), \
+                patch('salt.utils.files.fopen',
+                      mock_open(read_data=file_content)), \
+                patch('salt.utils.atomicfile.atomic_open',
+                      mock_open()) as atomic_open_mock:
+            filemod.line(name, content=cfg_content, before='exit 0', mode='ensure')
+            handles = atomic_open_mock.filehandles[name]
+            # We should only have opened the file once
+            open_count = len(handles)
+            assert open_count == 1, open_count
+            # We should only have invoked .writelines() once...
+            writelines_content = handles[0].writelines_calls
+            writelines_count = len(writelines_content)
+            assert writelines_count == 1, writelines_count
+            # ... with the updated content
+            expected = self._get_body(file_modified)
+            assert writelines_content[0] == expected, (writelines_content[0], expected)
+
+    @with_tempfile()
     def test_line_insert_ensure_before_first_line(self, name):
         '''
         Test for file.line for insertion ensuring the line is before first line
@@ -1573,6 +1613,44 @@ class FilemodLineTests(TestCase, LoaderModuleMockMixin):
         file_content = os.linesep.join([
             '#!/bin/bash',
             '/etc/init.d/someservice restart'
+        ])
+        file_modified = os.linesep.join([
+            '#!/bin/bash',
+            '/etc/init.d/someservice restart',
+            cfg_content
+        ])
+
+        isfile_mock = MagicMock(side_effect=lambda x: True if x == name else DEFAULT)
+        with patch('os.path.isfile', isfile_mock), \
+                patch('os.stat', MagicMock(return_value=DummyStat())), \
+                patch('salt.utils.files.fopen',
+                      mock_open(read_data=file_content)), \
+                patch('salt.utils.atomicfile.atomic_open',
+                      mock_open()) as atomic_open_mock:
+            filemod.line(name, content=cfg_content, after='/etc/init.d/someservice restart', mode='ensure')
+            handles = atomic_open_mock.filehandles[name]
+            # We should only have opened the file once
+            open_count = len(handles)
+            assert open_count == 1, open_count
+            # We should only have invoked .writelines() once...
+            writelines_content = handles[0].writelines_calls
+            writelines_count = len(writelines_content)
+            assert writelines_count == 1, writelines_count
+            # ... with the updated content
+            expected = self._get_body(file_modified)
+            assert writelines_content[0] == expected, (writelines_content[0], expected)
+
+    @with_tempfile()
+    def test_line_insert_duplicate_ensure_after(self, name):
+        '''
+        Test for file.line for insertion ensuring the line is after
+        :return:
+        '''
+        cfg_content = 'exit 0'
+        file_content = os.linesep.join([
+            '#!/bin/bash',
+            '/etc/init.d/someservice restart',
+            cfg_content
         ])
         file_modified = os.linesep.join([
             '#!/bin/bash',
